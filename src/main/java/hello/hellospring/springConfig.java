c22138a46de068dcb448384f9a67d4933ae6bfe9
=wookim <devwookim@gmail.com>
2023-06-15 19:27:41 +0900

    JDBC, Config

--- src/main/java/hello/hellospring/springConfig.java
+++ src/main/java/hello/hellospring/springConfig.java
@@ -1,14 +1,23 @@
 package hello.hellospring;
 //자바 코드로 직접 스프링 빈 등록하기
 
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class springConfig {
+
+    private final DataSource dataSource;
+
+    public springConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,7 +25,9 @@ public class springConfig {
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberRepository memberRepository()
+    {
+        //return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
